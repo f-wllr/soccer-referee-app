19eac38863b06e362163cf5f361d966217f70ecb
f-wllr <80111267+f-wllr@users.noreply.github.com>
2025-10-29 15:17:50 +0100

    fix error in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rcj_scoreboard/main.dart';
+import 'package:rcj_scoreboard/models/game.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    Game game = Game();
+    await tester.pumpWidget(MyApp(game: game,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
